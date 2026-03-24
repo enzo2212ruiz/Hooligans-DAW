4087ccd427a15bc8b13f0ffbb6208a67e1baffbc
Lucas Lobera <l.lobera19@gmail.com>
2026-03-24 12:36:57 +0100

    Pantalla login

---
+++ P.I-HOOLIGANSDAW/src/main/MainLogin.java
@@ -0,0 +1,9 @@
+package main;
+
+import vista.VentanaLogin;
+
+public class MainLogin {
+    public static void main(String[] args) {
+        new VentanaLogin();
+    }
+}
---
+++ P.I-HOOLIGANSDAW/src/vista/VentanaLogin.java
@@ -0,0 +1,92 @@
+package vista;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import java.awt.Font;
+import javax.swing.JTextField;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import javax.swing.SwingConstants;
+
+public class VentanaLogin extends JFrame {
+    
+    private JLabel lblTitulo = new JLabel("Pantalla Login");
+    private JLabel lblUsuario = new JLabel("Usuario");
+    private JLabel lblPassword = new JLabel("Password");
+    private JTextField textUsuario = new JTextField();
+    private JTextField textPassword = new JTextField();
+    private JButton btnLogin = new JButton("Login");
+    private JButton btnBorrar = new JButton("Borrar");
+    
+    public VentanaLogin() {
+        setBounds(500, 200, 250, 250);
+        setResizable(false);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        
+        configLabels();
+        configTextFields();
+        configButtons();
+        addPanelComponents();
+        
+        setVisible(true);
+    }
+    
+    private void configTextFields() {
+        textUsuario.setBounds(110, 71, 86, 20);
+        textUsuario.setColumns(10);
+        
+        textPassword.setBounds(110, 119, 86, 20);
+        textPassword.setColumns(10);
+    }
+    
+    private void configButtons() {
+        btnLogin.setBounds(24, 164, 89, 23);
+        
+        btnLogin.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                String usuario = textUsuario.getText();
+                String password = textPassword.getText();
+                
+                System.out.println(usuario);
+                System.out.println(password);
+                
+                setVisible(false);
+                
+                new Ventana(); // ← AQUÍ USA LA OTRA CLASE
+            }
+        });
+        
+        btnBorrar.setBounds(123, 164, 89, 23);
+        
+        btnBorrar.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                textPassword.setText("");
+                textUsuario.setText("");
+            }
+        });
+    }
+    
+    private void configLabels() {
+        lblUsuario.setFont(new Font("Tahoma", Font.PLAIN, 15));
+        lblUsuario.setBounds(24, 70, 64, 19);
+        
+        lblPassword.setFont(new Font("Tahoma", Font.PLAIN, 15));
+        lblPassword.setBounds(24, 120, 64, 19);
+
+        lblTitulo.setHorizontalAlignment(SwingConstants.CENTER);
+        lblTitulo.setFont(new Font("Tahoma", Font.PLAIN, 25));
+        lblTitulo.setBounds(10, 11, 209, 32);
+    }
+    
+    private void addPanelComponents() {
+        getContentPane().setLayout(null);
+        getContentPane().add(lblPassword);
+        getContentPane().add(lblUsuario);
+        getContentPane().add(textUsuario);
+        getContentPane().add(textPassword);
+        getContentPane().add(btnLogin);
+        getContentPane().add(btnBorrar);
+        getContentPane().add(lblTitulo);
+    }
+}
